44af9ef5868ae2fb1bb618cba22223684355a0fd
Aman Jain <aman.jain@gatech.edu>
2019-02-11 19:04:32 -0500

    added a file to try and figure out what flask blueprints are

--- src/app.py
+++ src/app.py
@@ -1,1 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/dddd')
+def aman():
+    return "hello, world! from aman."
+@app.route('/hi')
+def test():
+    return "hello"
+
