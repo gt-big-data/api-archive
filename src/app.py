7d2ddca01f23bd2f6105fee1c17ff939a57adeb1
unknown <brendenwaits@gatech.edu>
2019-02-18 18:05:05 -0500

    update filenames

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-from test import bus_wrapper 
+from buses import bus_wrapper 
 
 # Make the Flask app and connect the database
 app = Flask(__name__)
