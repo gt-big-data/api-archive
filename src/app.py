69b27b73a5d84e2cb45904c59e16891f83548017
Aman Jain <aman.jain@gatech.edu>
2019-02-25 18:55:07 -0500

    no more tests lmaooo

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from flask import request
 from marta.buses.marta_bus_wrapper import bus_wrapper
 from ping.testping import testping
+import unittest
 
 # Make the Flask app and connect the database
 app = Flask(__name__)
@@ -10,9 +11,3 @@ app.register_blueprint(bus_wrapper)
 app.register_blueprint(testping)
 CORS(app)
 
-@app.route("/aman",methods=['GET'])
-def aman():
-    if request.method == 'GET' :
-        return "hello"
-    else:
-        return "goodbye"
